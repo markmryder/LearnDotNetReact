dc00a16895ffb5b70c05bad8c06c6ee2ce3bd0ab
Mark Ryder <markmryder@gmail.com>
2021-01-17 15:56:50 -0500

    Initial data migration

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace API
 {
@@ -26,6 +28,12 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<DataContext>(opt =>
+            {
+
+                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Domain;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+		public DataContext(DbContextOptions options) : base(options)
+		{
+
+		}
+
+		public DbSet<Value> Values { get; set; }
+    }
+}
