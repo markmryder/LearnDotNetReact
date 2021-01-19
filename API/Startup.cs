5527518018d59b1d47efd81506a936aa9f3e9371
Mark Ryder <markmryder@gmail.com>
2021-01-19 16:38:08 -0500

    Added CORS to the startup class

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,6 +34,11 @@ namespace API
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
 
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +50,8 @@ namespace API
             }
 
             //app.UseHttpsRedirection();
+
+            app.UseCors("CorsPolicy");
 
             app.UseRouting();
 
