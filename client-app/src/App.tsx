0fdab657aecc579bb2190d87844fe3b9b543237b
Mark Ryder <markmryder@gmail.com>
2021-01-19 17:29:34 -0500

    Added semantic-ui to the project

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import {Header, Icon, List} from 'semantic-ui-react';
 
 class App extends Component{
 
@@ -22,15 +23,16 @@ class App extends Component{
 
   render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <ul>
-            {this.state.values.map((value:any) => (
-              <li key={value.id}>{value.name}:{value.id}</li>
-            ))}
-          </ul>
-        </header>
+      <div>
+          <Header as='h2'>
+            <Icon name='user'/>
+            <Header.Content>Reactivities</Header.Content>
+          </Header>
+          <List>
+            {this.state.values.map((value: any) => (
+              <List.Item key={value.id}>{value.name}</List.Item>
+            ))}          
+          </List>
       </div>
     );
   }
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'semantic-ui-css/semantic.min.css';
+//import 'semantic-ui-css';
 
 ReactDOM.render(
   
