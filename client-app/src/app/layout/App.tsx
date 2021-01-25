46211f4d131483f4fbd3e8314be4ab21791cd463
Mark Ryder <markmryder@gmail.com>
2021-01-24 21:52:44 -0500

    Added use of hooks for the activities

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react';
+import logo from './logo.svg';
+import axios from 'axios';
+import {Header, Icon, List} from 'semantic-ui-react';
+import { IActivity } from '../models/activity';
+
+// interface IState {
+//   activities: IActivity[]
+// }
+
+const App = () =>{
+
+  const [activities, setActivities] = useState<IActivity[]>([]);
+
+  useEffect(() => {
+    axios.get<IActivity[]>('http://localhost:5000/api/activities').then(response => {
+      setActivities(response.data);
+    });
+  }, []);
+  
+    return (
+      <div>
+          <Header as='h2'>
+            <Icon name='user'/>
+            <Header.Content>Reactivities</Header.Content>
+          </Header>
+          <List>
+            {activities.map((activity) => (
+              <List.Item key={activity.id}>{activity.title}</List.Item>
+            ))}          
+          </List>
+      </div>
+    );
+  
+}
+
+export default App;
