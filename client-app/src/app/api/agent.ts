90e7489c68dfb45aa58b3ce637b505243ef0cf72
Mark Ryder <markmryder@gmail.com>
2021-01-26 12:45:43 -0500

    Added api calls to App.tsx, and a load delay to agent.ts file for testing purposes

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -6,19 +6,22 @@ axios.defaults.baseURL = 'http://localhost:5000/api';
 const responseBody = (response: AxiosResponse) => response.data;
 
 const requests ={
-    get: (url: string) => axios.get(url).then(responseBody),
-    post: (url: string, body: {}) => axios.post(url,body).then(responseBody),
-    put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
-    del: (url: string) => axios.delete(url).then(responseBody)
+    get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
+    post: (url: string, body: {}) => axios.post(url,body).then(sleep(1000)).then(responseBody),
+    put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
+    del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody)
 }
 
 const Activities = {
-    list: () => requests.get('/activities'),
+    list: (): Promise<IActivity[]> => requests.get('/activities'),
     details: (id: string) => requests.get(`/activities/${id}`),
     create: (activity: IActivity) => requests.post('/activities',activity),
     update: (activity: IActivity) => requests.put(`/activities/${activity.id}`,activity),
     delete: (id: string) => requests.del(`/activities/${id}`)
 }
+
+const sleep = (ms: number) => (response: AxiosResponse) => 
+    new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms))
 
 export default {
     Activities
